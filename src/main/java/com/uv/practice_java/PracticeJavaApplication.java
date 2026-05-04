b7eba48bcd48679b323fc805794b59a3e1458153
vaibhavzalte <vaibhavzalte1004@gmail.com>
2026-05-04 11:16:08 +0530

    initial folder setup

---
+++ src/main/java/com/uv/practice_java/PracticeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.uv.practice_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uv/practice_java/PracticeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uv.practice_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
