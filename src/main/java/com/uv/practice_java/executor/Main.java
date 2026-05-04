63ae0d75bac759293b86ff16a35645388df0bbfe
vaibhavzalte <vaibhavzalte1004@gmail.com>
2026-05-04 12:48:39 +0530

    Executor

--- src/main/java/com/uv/practice_java/executor/Main.java
+++ src/main/java/com/uv/practice_java/executor/Main.java
@@ -1,23 +1,21 @@
 package com.uv.practice_java.executor;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Thread[] threads = new Thread[15];
+        Executor executorService = Executors.newFixedThreadPool(15);
 
         for (int i = 0; i < 15; i++) {
             int finalI = i;
-            threads[i] = new Thread(() -> {
+            executorService.execute(() -> {
                 System.out.println("Factorial of " + finalI + " is " + factorial(finalI));
             });
         }
-
-        for (Thread thread : threads) {
-            thread.start();
-        }
-
-        for (Thread thread : threads) {
-            thread.join();
-        }
+        System.out.println("All tasks submitted. - it will print first");
     }
 
     public static long factorial(int n) {
