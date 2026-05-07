6b45e0337e9b555f9baa315feb9279b0c5682b03
vaibhavzalte <vaibhavzalte1004@gmail.com>
2026-05-07 13:20:44 +0530

    synchronized block

--- src/main/java/com/uv/practice_java/multithreading/ThreadCommunication.java
+++ src/main/java/com/uv/practice_java/multithreading/ThreadCommunication.java
@@ -3,53 +3,25 @@ package com.uv.practice_java.multithreading;
 import java.util.List;
 
 public class ThreadCommunication {
-    public static void main(String[] args) {
-        Restaurant restaurant = new Restaurant();
-        Thread customerThread = new Thread(() -> {
-            restaurant.placeOrder("Pizza");
-            restaurant.placeOrder("Burger");
-            restaurant.placeOrder("Pasta");
-        });
-        Thread chefThread = new Thread(() -> {
-            try {
-                restaurant.serveOrder();
-                restaurant.serveOrder();
-                restaurant.serveOrder();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        customerThread.start();
-        chefThread.start();
-    }
+    public static void   main(String[] args) {
+        Object lock = new Object();
+       Runnable r = () ->{
+           System.out.println("step-1 : "+Thread.currentThread().getName());
+           synchronized (lock){
+               try {
+                   Thread.sleep(2000);
+               System.out.println("step-2 : critical section : "+Thread.currentThread().getName());
+               } catch (InterruptedException e) {
+                   throw new RuntimeException(e);
+               }
+           }
+           System.out.println("step-3 : "+Thread.currentThread().getName());
+       };
+       Thread t1 = new Thread(r);
+       Thread t2 = new Thread(r);
+       Thread t3 = new Thread(r);
+       t1.start();
+         t2.start();
+            t3.start();
 }
-class Restaurant {
-    private List<String> orders;
-    private int capacity=2;
-    Restaurant(){
-        orders = new java.util.LinkedList<>();
-    }
-    public synchronized void placeOrder(String order){
-        while (orders.size()==capacity)
-        {
-            try {
-                System.out.println("Restaurant is full, waiting for orders to be processed...");
-                wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        System.out.println("Placing order: " + order);
-        orders.add(order);
-        notifyAll();
-    }
-    public synchronized void serveOrder() throws InterruptedException {
-        while (orders.isEmpty()){
-            System.out.println("No food to serve, waiting for orders...");
-            wait();
-        }
-        String order = orders.remove(0);
-        System.out.println("Serving order: " + order);
-        notifyAll();
-    }
 }
